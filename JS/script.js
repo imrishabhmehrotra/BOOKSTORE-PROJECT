359fdadbc87b4ec4d36d5889c30afb4d79239901
Rishabh Mehrotra <rishabh.2024en1164@kiet.edu>
2023-12-25 01:23:40 +0530

    Completed 1st session

---
+++ JS/script.js
@@ -0,0 +1,9 @@
+window.onscroll = () => {
+    if(window.scrollY > 80){
+        document.querySelector(".header .header-2").classList.add("active");
+    }else{
+        document.querySelector(".header .header-2").classList.add("active");
+
+    }
+}
+
