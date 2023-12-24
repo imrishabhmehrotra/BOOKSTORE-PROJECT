2be2a00ca23c08d995a266ea6e85cb7ba2cdd892
Rishabh Mehrotra <rishabh.2024en1164@kiet.edu>
2023-12-25 01:30:43 +0530

    Added Icons

--- JS/script.js
+++ JS/script.js
@@ -7,3 +7,15 @@ window.onscroll = () => {
     }
 }
 
+window.onscroll = () => {
+  if (window.scrollY > 80) {
+    document.querySelector(".header .header-2").classList.add("active");
+  } else {
+    document.querySelector(".header .header-2").classList.add("active");
+  }
+};
+
+
+
+
+
